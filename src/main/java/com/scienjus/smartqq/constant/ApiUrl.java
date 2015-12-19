e47f38e6ce07e999657618390fd4b4a343c4c318
ScienJus <xie_enlong@foxmail.com>
2015-12-19 19:10:25 +0800

    Api的请求地址和referer枚举

---
+++ src/main/java/com/scienjus/smartqq/constant/ApiUrl.java
@@ -0,0 +1,83 @@
+package com.scienjus.smartqq.constant;
+
+/**
+ * Api的请求地址和Referer
+ * @author ScienJus
+ * @date 15/12/19.
+ */
+public enum ApiUrl {
+
+    GET_QE_CODE(
+            "https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t=0.1",
+            null
+    ),
+    VERIFY_QR_CODE(
+            "https://ssl.ptlogin2.qq.com/ptqrlogin?" +
+                    "webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&" +
+                    "u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&" +
+                    "ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&" +
+                    "action=0-0-157510&mibao_css=m_webqq&t=1&g=1&js_type=0&js_ver=10143&login_sig=&pt_randsalt=0",
+            "https://ui.ptlogin2.qq.com/cgi-bin/login?" +
+                    "daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&" +
+                    "s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
+    ),
+    GET_PTWEBQQ(
+            "{1}",
+            null
+    ),
+    GET_VFWEBQQ(
+            "http://s.web2.qq.com/api/getvfwebqq?ptwebqq={1}&clientid=53999199&psessionid=&t=0.1",
+            "http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1"
+    ),
+    GET_UIN_AND_PSESSIONID(
+            "http://d1.web2.qq.com/channel/login2",
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
+    ),
+    GET_GROUP_LIST(
+            "http://s.web2.qq.com/api/get_group_name_list_mask2",
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
+    ),
+    POLL_MESSAGE(
+            "http://d1.web2.qq.com/channel/poll2",
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
+    ),
+    SEND_MESSAGE_TO_GROUP(
+            "http://d1.web2.qq.com/channel/send_qun_msg2",
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
+    ),
+    GET_FRIEND_LIST(
+            "http://s.web2.qq.com/api/get_user_friends2",
+            "http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1"
+    ),
+    SEND_MESSAGE_TO_FRIEND(
+            "http://d1.web2.qq.com/channel/send_buddy_msg2",
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
+    );
+
+    private String url;
+
+    private String referer;
+
+    ApiUrl(String url, String referer) {
+        this.url = url;
+        this.referer = referer;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+
+    public String getReferer() {
+        return referer;
+    }
+
+    public String buildUrl(Object... params) {
+        int i = 1;
+        String url = this.url;
+        for (Object param : params) {
+            url = url.replace("{" + i + "}", param.toString());
+        }
+        return url;
+    }
+}
