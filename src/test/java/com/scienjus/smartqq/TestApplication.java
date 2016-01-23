5ef64e4253dd9d1591801875c9c3a377520e9902
wsdjeg <wsdjeg@outlook.com>
2016-01-23 22:47:52 +0800

    Add project.json

---
+++ src/test/java/com/scienjus/smartqq/TestApplication.java
@@ -0,0 +1,10 @@
+package com.scienjus.smartqq;
+
+import com.scienjus.smartqq.Application;
+
+public class TestApplication {
+    public static void run() {
+        String[] args = {};
+        Application.main(args);
+    }
+}
