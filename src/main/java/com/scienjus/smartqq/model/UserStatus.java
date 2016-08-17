2b89c82f9b3e1a962be93c4ef6eab70e564d4db6
Xianguang-Zhou <xianguang.zhou@outlook.com>
2016-08-17 12:01:27 +0800

    add method "UserStatus.fromCode"

--- src/main/java/com/scienjus/smartqq/model/UserStatus.java
+++ src/main/java/com/scienjus/smartqq/model/UserStatus.java
@@ -1,5 +1,8 @@
 package com.scienjus.smartqq.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * QQ user status
  *
@@ -7,28 +10,41 @@ package com.scienjus.smartqq.model;
  * @date 2016/08/02.
  */
 public enum UserStatus {
-    //在线
-    ONLINE("online"),
-    //Q我
-    CALLME("callme"),
-    //离开
-    AWAY("away"),
-    //忙碌
-    BUSY("busy"),
-    //静音
-    SILENT("silent"),
-    //隐身
-    HIDDEN("hidden"),
-    //离线
-    OFFLINE("offline");
+	// 在线
+	ONLINE("online"),
+	// Q我
+	CALLME("callme"),
+	// 离开
+	AWAY("away"),
+	// 忙碌
+	BUSY("busy"),
+	// 静音
+	SILENT("silent"),
+	// 隐身
+	HIDDEN("hidden"),
+	// 离线
+	OFFLINE("offline");
 
-    private String statusCode;
+	private String statusCode;
 
-    private UserStatus(String statusCode) {
-        this.statusCode = statusCode;
-    }
+	private UserStatus(String statusCode) {
+		this.statusCode = statusCode;
+	}
 
-    public String getStatusCode() {
-        return statusCode;
-    }
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	private final static Map<String, UserStatus> codeToStatus = new HashMap<>();
+
+	static {
+		for (UserStatus userStatus : UserStatus.values()) {
+			codeToStatus.put(userStatus.statusCode, userStatus);
+		}
+	}
+
+	public static UserStatus fromCode(String code) {
+		return codeToStatus.get(code);
+	}
+
 }
