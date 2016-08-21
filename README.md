5dc8b933574b4b0ff43a4babbe5f32654daf7025
Xianguang-Zhou <xianguang.zhou@outlook.com>
2016-08-21 11:55:16 +0800

    change test code and README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Smart QQ（WebQQ）的API，从https://github.com/ScienJus/smartqq 修改而来
 
-该项目目前（2016年8月2日）为止还可以正常使用，我也会尽量一直维护这个项目。
+该项目目前（2016年8月21日）为止还可以正常使用，我也会尽量一直维护这个项目。
 
 该项目仅提供了最基本的通信协议，你可以在此基础上实现自己的业务逻辑，包括且不限于：
 
@@ -49,54 +49,61 @@ mvn clean source:jar install
 ```
 public class Application {
 
-    public static void main(String[] args) throws Exception {
-        //创建一个新对象时需要扫描二维码登录，并且传一个处理接收到消息的回调，如果你不需要接收消息，可以传null
-        SmartQQClient client = new SmartQQClient(new SmartqqListener() {
-            @Override
-            public void onMessage(Message message) {
-                System.out.println(message.getContent());
-            }
+	public static void main(String[] args) throws Exception {
+		// 创建一个新对象时需要扫描二维码登录，并且传一个处理接收到消息的监听器
+		@SuppressWarnings("resource")
+		SmartQQClient client = new SmartQQClient(new SmartqqListener() {
+			@Override
+			public void onMessage(Message message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onGroupMessage(GroupMessage message) {
-                System.out.println(message.getContent());
-            }
+			@Override
+			public void onGroupMessage(GroupMessage message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onDiscussMessage(DiscussMessage message) {
-                System.out.println(message.getContent());
-            }
+			@Override
+			public void onDiscussMessage(DiscussMessage message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onQrCodeImage(byte[] imageBytes) {
-                //本地存储二维码图片
-                try {
-                    File imageFile = new File("qrcode.png");
-                    try (FileOutputStream fos = new FileOutputStream(imageFile)) {
-                        fos.write(imageBytes);
-                    }
-                    String filePath = imageFile.getCanonicalPath();
-                    System.out.println("二维码已保存在 " + filePath + " 文件中，请打开手机QQ并扫描二维码");
-                } catch (IOException e) {
-                    throw new IllegalStateException("二维码保存失败");
-                }
-            }
-        });
-        //登录成功后便可以编写你自己的业务逻辑了
-        List<Category> categories = client.getFriendListWithCategory();
-        for (Category category : categories) {
-            System.out.println(category.getName());
-            for (Friend friend : category.getFriends()) {
-                System.out.println("————" + friend.getNickname());
-            }
-        }
-        //使用后调用close方法关闭，你也可以使用try-with-resource创建该对象并自动关闭
-        try {
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			@Override
+			public void onQrCodeImage(byte[] imageBytes) {
+				// 本地存储二维码图片
+				try {
+					File imageFile = new File("qrcode.png");
+					try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+						fos.write(imageBytes);
+					}
+					String filePath = imageFile.getCanonicalPath();
+					System.out.println("二维码已保存在 " + filePath + " 文件中，请打开手机QQ并扫描二维码");
+				} catch (IOException e) {
+					System.out.println("二维码保存失败：");
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void onException(Throwable exception, ExceptionThreadType exceptionThreadType) {
+			}
+		});
+		try {
+			// 登录成功后便可以编写你自己的业务逻辑了
+			List<Category> categories = client.getFriendListWithCategory();
+			for (Category category : categories) {
+				System.out.println(category.getName());
+				for (Friend friend : category.getFriends()) {
+					System.out.println("————" + friend.getNickname());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 使用后调用close或closeNow方法关闭，你也可以使用try-with-resource创建该对象并自动关闭
+			client.closeNow();
+		}
+	}
 }
 ```
 
--- src/test/java/com/scienjus/smartqq/Application.java
+++ src/test/java/com/scienjus/smartqq/Application.java
@@ -1,13 +1,18 @@
 package com.scienjus.smartqq;
 
-import com.scienjus.smartqq.client.SmartQQClient;
-import com.scienjus.smartqq.listener.SmartqqListener;
-import com.scienjus.smartqq.model.*;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+
+import com.scienjus.smartqq.client.SmartQQClient;
+import com.scienjus.smartqq.listener.ExceptionThreadType;
+import com.scienjus.smartqq.listener.SmartqqListener;
+import com.scienjus.smartqq.model.Category;
+import com.scienjus.smartqq.model.DiscussMessage;
+import com.scienjus.smartqq.model.Friend;
+import com.scienjus.smartqq.model.GroupMessage;
+import com.scienjus.smartqq.model.Message;
 
 /**
  * @author ScienJus
@@ -16,52 +21,59 @@ import java.util.List;
  */
 public class Application {
 
-    public static void main(String[] args) throws Exception {
-        //创建一个新对象时需要扫描二维码登录，并且传一个处理接收到消息的回调，如果你不需要接收消息，可以传null
-        SmartQQClient client = new SmartQQClient(new SmartqqListener() {
-            @Override
-            public void onMessage(Message message) {
-                System.out.println(message.getContent());
-            }
+	public static void main(String[] args) throws Exception {
+		// 创建一个新对象时需要扫描二维码登录，并且传一个处理接收到消息的监听器
+		@SuppressWarnings("resource")
+		SmartQQClient client = new SmartQQClient(new SmartqqListener() {
+			@Override
+			public void onMessage(Message message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onGroupMessage(GroupMessage message) {
-                System.out.println(message.getContent());
-            }
+			@Override
+			public void onGroupMessage(GroupMessage message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onDiscussMessage(DiscussMessage message) {
-                System.out.println(message.getContent());
-            }
+			@Override
+			public void onDiscussMessage(DiscussMessage message) {
+				System.out.println(message.getContent());
+			}
 
-            @Override
-            public void onQrCodeImage(byte[] imageBytes) {
-                //本地存储二维码图片
-                try {
-                    File imageFile = new File("qrcode.png");
-                    try (FileOutputStream fos = new FileOutputStream(imageFile)) {
-                        fos.write(imageBytes);
-                    }
-                    String filePath = imageFile.getCanonicalPath();
-                    System.out.println("二维码已保存在 " + filePath + " 文件中，请打开手机QQ并扫描二维码");
-                } catch (IOException e) {
-                    throw new IllegalStateException("二维码保存失败");
-                }
-            }
-        });
-        //登录成功后便可以编写你自己的业务逻辑了
-        List<Category> categories = client.getFriendListWithCategory();
-        for (Category category : categories) {
-            System.out.println(category.getName());
-            for (Friend friend : category.getFriends()) {
-                System.out.println("————" + friend.getNickname());
-            }
-        }
-        //使用后调用close或closeNow方法关闭，你也可以使用try-with-resource创建该对象并自动关闭
-        try {
-            client.closeNow();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			@Override
+			public void onQrCodeImage(byte[] imageBytes) {
+				// 本地存储二维码图片
+				try {
+					File imageFile = new File("qrcode.png");
+					try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+						fos.write(imageBytes);
+					}
+					String filePath = imageFile.getCanonicalPath();
+					System.out.println("二维码已保存在 " + filePath + " 文件中，请打开手机QQ并扫描二维码");
+				} catch (IOException e) {
+					System.out.println("二维码保存失败：");
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void onException(Throwable exception, ExceptionThreadType exceptionThreadType) {
+			}
+		});
+		try {
+			// 登录成功后便可以编写你自己的业务逻辑了
+			List<Category> categories = client.getFriendListWithCategory();
+			for (Category category : categories) {
+				System.out.println(category.getName());
+				for (Friend friend : category.getFriends()) {
+					System.out.println("————" + friend.getNickname());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 使用后调用close或closeNow方法关闭，你也可以使用try-with-resource创建该对象并自动关闭
+			client.closeNow();
+		}
+	}
 }
