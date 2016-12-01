953fe905e196d420b716737bd67825d2d26051d8
lonhay <448540132@qq.com>
2016-12-01 17:53:33 +0800

    update urls

--- src/main/java/com/scienjus/smartqq/constant/ApiURL.java
+++ src/main/java/com/scienjus/smartqq/constant/ApiURL.java
@@ -8,25 +8,19 @@ package com.scienjus.smartqq.constant;
 public enum ApiURL {
 
     GET_QR_CODE(
-            "https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t=0.1",
-            ""
+            "https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t={1}",
+            "https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http://w.qq.com/proxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
     ),
     VERIFY_QR_CODE(
-            "https://ssl.ptlogin2.qq.com/ptqrlogin?" +
-                    "webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&" +
-                    "u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&" +
-                    "ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&" +
-                    "action=0-0-157510&mibao_css=m_webqq&t=1&g=1&js_type=0&js_ver=10143&login_sig=&pt_randsalt=0",
-            "https://ui.ptlogin2.qq.com/cgi-bin/login?" +
-                    "daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&" +
-                    "s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
+            "https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-2146&mibao_css=m_webqq&t=1&g=1&js_type=0&js_ver=10184&login_sig=&pt_randsalt=3",
+            "https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
     ),
     GET_PTWEBQQ(
             "{1}",
             null
     ),
     GET_VFWEBQQ(
-            "http://s.web2.qq.com/api/getvfwebqq?ptwebqq={1}&clientid=53999199&psessionid=&t=0.1",
+            "http://s.web2.qq.com/api/getvfwebqq?ptwebqq={1}&clientid=53999199&psessionid=&t={2}",
             "http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1"
     ),
     GET_UIN_AND_PSESSIONID(
